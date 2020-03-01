d077c215620f3ca7f305ef77622f1e8ce136d378
studyHooligen <2373180028@qq.com>
2020-03-01 21:44:49 +0800

    添加到ch6代码

--- buildFiles/cmake_install.cmake
+++ buildFiles/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/studyhooligen/slam/20202
+# Install script for directory: /home/studyhooligen/slam2020/CH6
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/studyhooligen/slam/buildFiles/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/studyhooligen/slam2020/buildFiles/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
